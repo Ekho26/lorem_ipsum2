76ae0c20108f063aaee20da1a8f50d406087ff59
Ekho26 <munteanu.ionut1991@gmail.com>
2021-04-07 12:47:24 +0300

    Adding the Overlay for the button action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,46 @@
-import React from 'react'
+import React from "react";
 
 // Styles
-import '../scss/style.css'
+import "../scss/style.css";
 
 function Navbar() {
-    return (
-      <div>
-        <header className="header">
-          <nav className="flex flex-jc-sb flex-ai-c">
-            <a
-              href="/"
-              className="header__logo flex flex-jc-sb flex-ai-c header__links"
-            >
-              <img src="./logo192.png" alt="Lorem Ipsum" />
-              <a href="#">Lorem Ipsum</a>
-            </a>
-            <div className="flex flex-ai-c">
-              <a href="#" className="header__menu hide-for-desktop">
+  return (
+    <div>
+      <header className="header">
+        <div className="overlay"></div>
+        <nav className="flex flex-jc-sb flex-ai-c">
+          <a
+            href="/"
+            className="header__logo flex flex-jc-sb flex-ai-c header__links"
+          >
+            <img src="./logo192.png" alt="Lorem Ipsum" />
+            <p className="hide-for-mobile">Lorem Ipsum</p>
+          </a>
+          <div className="flex flex-ai-c">
+            <div className="flex flex-jc-sb flex-ai-c header__links hide-for-desktop">
+              <a href="#" className="header__menu">
                 <span></span>
                 <span></span>
                 <span></span>
               </a>
-              <div className="header__links hide-for-mobile">
-                <a href="#">HOME</a>
-                <a href="#">FEATURES</a>
-                <a href="#">ABOUT US</a>
-                <a href="#">LOG IN</a>
-              </div>
-              <a href="#" className="button header__cta hide-for-mobile">
-                GET DEMO
+              <a className="header__logo flex flex-jc-sb flex-ai-c header__links">
+                MENU
               </a>
             </div>
-          </nav>
-        </header>
-      </div>
-    );
+            <div className="header__links hide-for-mobile">
+              <a href="#">HOME</a>
+              <a href="#">FEATURES</a>
+              <a href="#">ABOUT US</a>
+              <a href="#">LOG IN</a>
+            </div>
+            <a href="#" className="button header__cta">
+              GET DEMO
+            </a>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Navbar;
