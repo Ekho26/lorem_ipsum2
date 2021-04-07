4ab66c95b3f4421bd87b23cf9734dd274a90896f
Ekho26 <munteanu.ionut1991@gmail.com>
2021-04-07 11:45:25 +0300

    Finished Desktop Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,36 @@ import '../scss/style.css'
 
 function Navbar() {
     return (
-        <div>
-            <header className="header">
-                <nav className="flex flex-jc-sb flex-ai-c">
-                    <a href="/" className="header__logo">
-                        <img src="./logo192.png" alt="Lorem Ipsum" />
-                    </a>
-                    <a href="#" className="header__menu">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                    </a>
-                    <div className="header__links hide-for-mobile">
-                        <a href="#">HOME</a>
-                        <a href="#">FEATURES</a>
-                        <a href="#">ABOUT US</a>
-                        <a href="#">LOG IN</a>
-                    </div>
-                    <a href="#" className="button header__cta hide-for-mobile">GET DEMO</a>
-                </nav>
-            </header>
-        </div>
-    )
+      <div>
+        <header className="header">
+          <nav className="flex flex-jc-sb flex-ai-c">
+            <a
+              href="/"
+              className="header__logo flex flex-jc-sb flex-ai-c header__links"
+            >
+              <img src="./logo192.png" alt="Lorem Ipsum" />
+              <a href="#">Lorem Ipsum</a>
+            </a>
+            <div className="flex flex-ai-c">
+              <a href="#" className="header__menu hide-for-desktop">
+                <span></span>
+                <span></span>
+                <span></span>
+              </a>
+              <div className="header__links hide-for-mobile">
+                <a href="#">HOME</a>
+                <a href="#">FEATURES</a>
+                <a href="#">ABOUT US</a>
+                <a href="#">LOG IN</a>
+              </div>
+              <a href="#" className="button header__cta hide-for-mobile">
+                GET DEMO
+              </a>
+            </div>
+          </nav>
+        </header>
+      </div>
+    );
 }
 
 export default Navbar;
